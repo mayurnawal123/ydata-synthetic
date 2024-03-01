0d2e362f0c0b98752c1604f8cbdd8afe380cec78
mayurnawal123 <56102443+mayurnawal123@users.noreply.github.com>
2024-03-01 22:44:40 +0400

    Update streamlit_app.py
    installed y data synthetic

--- examples/streamlit_app.py
+++ examples/streamlit_app.py
@@ -1,6 +1,8 @@
 """
     Python file example with the script to run ydata-synthetic streamlit app
 """
+pip install ydata-synthetic[streamlit]
+
 from ydata_synthetic import streamlit_app
 
 if __name__ == '__main__':
